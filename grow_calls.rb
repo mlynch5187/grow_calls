8125d9a2025263f97c81f83323877f730f20a0ff
Alex Lynch <mlynch5187@gmail.com>
2020-09-12 11:20:52 -0600

    modify sinatra api calls

--- grow_calls.rb
+++ grow_calls.rb
@@ -12,16 +12,15 @@ require './opendata_service.rb'
     content_type :json
     coordinates_conn = Faraday.new("https://public.opendatasoft.com")
     coordinates_response = coordinates_conn.get("/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=#{params[:zip]}")
-    json = JSON.parse(coordinates_response.body, symbolize_names: true)
+    coordinates_json = JSON.parse(coordinates_response.body, symbolize_names: true)
     coordinates_data = {}
-    coordinates_data[:latitude] = json[:records][0][:fields][:latitude]
-    coordinates_data[:longitude] = json[:records][0][:fields][:longitude]
+    coordinates_data[:latitude] = coordinates_json[:records][0][:fields][:latitude]
+    coordinates_data[:longitude] = coordinates_json[:records][0][:fields][:longitude]
     coordinates_data
 
     ph_conn = Faraday.new("https://rest.soilgrids.org")
     ph_response = ph_conn.get("/soilgrids/v2.0/properties/query?lon=#{coordinates_data[:longitude]}&lat=#{coordinates_data[:latitude]}&property=phh2o")
-    json = JSON.parse(ph_response.body, symbolize_names: true)
-    ph_data = json[:properties][:layers][0][:depths][1][:values][:mean].to_f / 10
-
-    require "pry"; binding.pry
+    ph_json = JSON.parse(ph_response.body, symbolize_names: true)
+    ph_data = ph_json[:properties][:layers][0][:depths][1][:values][:mean].to_f / 10
+    ph_data
   end
