8b6fe070ea01a91333f4d281c4994c25d39aa848
Alex Lynch <mlynch5187@gmail.com>
2020-09-12 10:37:40 -0600

    Retrieve soil ph with longitude/latitude
    Co-authored-by: Ryan Laleh <33795357+RyN21@users.noreply.github.com>
    Co-authored-by: Nick Edwin <61946178+NickEdwin@users.noreply.github.com>
    Co-authored-by: Chandler Hulstrom <62038636+Chulstro@users.noreply.github.com>

--- grow_calls.rb
+++ grow_calls.rb
@@ -10,13 +10,18 @@ require './opendata_service.rb'
 
   get '/ph' do
     content_type :json
-    conn = Faraday.new("https://public.opendatasoft.com")
-    response = conn.get("/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=#{params[:zip]}")
-    json = JSON.parse(response.body, symbolize_names: true)
-    data = {}
-    data[:latitude] = json[:records][0][:fields][:latitude]
-    data[:longitude] = json[:records][0][:fields][:longitude]
-    data
+    coordinates_conn = Faraday.new("https://public.opendatasoft.com")
+    coordinates_response = coordinates_conn.get("/api/records/1.0/search/?dataset=us-zip-code-latitude-and-longitude&q=#{params[:zip]}")
+    json = JSON.parse(coordinates_response.body, symbolize_names: true)
+    coordinates_data = {}
+    coordinates_data[:latitude] = json[:records][0][:fields][:latitude]
+    coordinates_data[:longitude] = json[:records][0][:fields][:longitude]
+    coordinates_data
 
-    conn_2 = Faraday.new("")
+    ph_conn = Faraday.new("https://rest.soilgrids.org")
+    ph_response = ph_conn.get("/soilgrids/v2.0/properties/query?lon=#{coordinates_data[:longitude]}&lat=#{coordinates_data[:latitude]}&property=phh2o")
+    json = JSON.parse(ph_response.body, symbolize_names: true)
+    ph_data = json[:properties][:layers][0][:depths][1][:values][:mean].to_f / 10
+
+    require "pry"; binding.pry
   end
