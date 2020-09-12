0e89a0bef468b31fb39cc143806e1fee9176ec36
Nick Edwin <nickedwin85@gmail.com>
2020-09-12 14:14:20 -0600

    Updated to send approprate json response

--- grow_calls.rb
+++ grow_calls.rb
@@ -16,12 +16,11 @@ class GrowCalls < Sinatra::Base
     coordinates_data = {}
     coordinates_data[:latitude] = coordinates_json[:records][0][:fields][:latitude]
     coordinates_data[:longitude] = coordinates_json[:records][0][:fields][:longitude]
-    coordinates_data
 
     ph_conn = Faraday.new("https://rest.soilgrids.org")
     ph_response = ph_conn.get("/soilgrids/v2.0/properties/query?lon=#{coordinates_data[:longitude]}&lat=#{coordinates_data[:latitude]}&property=phh2o")
     ph_json = JSON.parse(ph_response.body, symbolize_names: true)
-    ph_data = ph_json[:properties][:layers][0][:depths][1][:values][:mean].to_f / 10
-    ph_data
+    # ph_data = ph_json[:properties][:layers][0][:depths][1][:values][:mean].to_f / 10
+    ph_json.to_json
   end
 end
